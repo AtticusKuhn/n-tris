85bb1e3a291f6d01f7ffe81216ba1f43c4e1b01e
eulerthedestroyer <eulerthedestroyer@gmail.com>
2022-01-14 16:38:38 -0800

    wokring

--- index.js
+++ index.js
@@ -1,60 +1,188 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 var input = document.getElementById("input");
-var pixel = 10;
-var width = 500;
-var height = 500;
-var pixelWidth = 500 / pixel;
-var pixelhHeight = 500 / pixel;
+var pixel = 20;
+var pixelWidth = 10;
+var pixelHeight = 24;
+var width = pixelWidth * pixel;
+var height = pixelHeight * pixel;
+var momevement = 3;
 canvas.width = width;
 canvas.height = height;
-var generatePiece = function () {
+var generateShape = function (n) {
+    if (n === 1) {
+        return [[true]];
+    }
+    else {
+        var s = generateShape(n - 1);
+        if (Math.random() > 1 / n) {
+            s[Math.floor(Math.random() * s.length)].push(true);
+        }
+        else {
+            s.push([true]);
+        }
+        return s;
+    }
+};
+var generatePiece = function (n) {
+    if (n === void 0) { n = 4; }
     return {
         position: {
             x: 0,
             y: 0
         },
-        shape: [
-            // [true, true, true],
-            [true, true, true, true, true, true, true]
-        ]
+        shape: generateShape(n)
     };
 };
 var currentPiece = generatePiece();
-var makeArray = function (length, element) { return new Array(length).fill(element); };
-var board = makeArray(pixelWidth, makeArray(pixelhHeight, false));
-function draw() {
-    var ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    board.forEach(function (xrow, xindex) {
-        xrow.forEach(function (y, yindex) {
-            // const x = xrow[xindex];
-            if (y)
-                ctx.fillRect(xindex * pixel, yindex * pixel, pixel, pixel);
-            ctx.fillStyle = "hsl(" + (yindex * pixel / pixelWidth) * 20 + ", " + (xindex * pixel / pixelWidth) * 2 + "%, 50%)";
-        });
-    });
-    ctx.fillStyle = "red";
-    currentPiece.shape.forEach(function (row, rowIndex) {
-        row.forEach(function (column, columnIndex) {
-            if (column)
-                ctx.fillRect(currentPiece.position.x + columnIndex * pixel, currentPiece.position.y + rowIndex * pixel, pixel, pixel);
-        });
-    });
-    ctx.fillStyle = "black";
-    return canvas;
-}
-function tick() {
-    if (currentPiece.position.y / pixel + pixel > pixelhHeight) {
-        currentPiece = generatePiece();
+var mod = function (num, base) {
+    if (num < 0) {
+        return mod(num + base, base);
     }
     else {
-        currentPiece.position.y += pixel;
+        return num % base;
+    }
+};
+var safeGet = function (array, index) {
+    if (array.length === 0) {
+        throw new Error("cannot get an empty array");
+    }
+    return array[mod(Math.floor(index), array.length)];
+};
+var clone = function (x) {
+    if (Array.isArray(x)) {
+        //@ts-ignore
+        return __spreadArray([], x, true);
+    }
+    else if (typeof x === "object") {
+        return __assign({}, x);
+    }
+    else {
+        return x;
+    }
+};
+var keys = {
+    "w": false,
+    "a": false,
+    s: false,
+    d: false
+};
+window.addEventListener("keydown", function (keyboardEvent) {
+    var key = keyboardEvent.key;
+    if (Object.keys(keys).includes(keyboardEvent.key)) {
+        keys[key] = true;
+    }
+});
+window.addEventListener("keyup", function (keyboardEvent) {
+    var key = keyboardEvent.key;
+    if (Object.keys(keys).includes(keyboardEvent.key)) {
+        keys[key] = false;
+    }
+});
+var makeArray = function (length, element) {
+    var x = [];
+    for (var i = 0; i < length; i++) {
+        x.push(clone(element));
+    }
+    console.log(x);
+    return x;
+};
+var drawPixel = function (x, y) { return ctx.fillRect(x, y, pixel, pixel); };
+var makeBoard = function () { return makeArray(pixelWidth, __spreadArray(__spreadArray([], makeArray(pixelHeight - 1, false), true), makeArray(pixelWidth, true), true)); };
+var board = makeBoard();
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    iterateCoveringArray(board, function (xindex, yindex) {
+        ctx.fillStyle = "hsl(" + (yindex / pixelHeight) * 360 + ", " + (xindex / pixelWidth) * 100 + "%, 60%)";
+        drawPixel(xindex * pixel, yindex * pixel);
+    });
+    // ctx.fillStyle = "red"
+    iterateCoveringArray(currentPiece.shape, function (columnIndex, rowIndex) {
+        return drawPixel(currentPiece.position.x + columnIndex * pixel, currentPiece.position.y + rowIndex * pixel);
+    });
+}
+var iterateCoveringArray = function (piece, func) {
+    if (!Array.isArray(piece))
+        throw new Error("iterateCoveringArray expected an array but got " + piece);
+    piece.forEach(function (row, xindex) {
+        if (!Array.isArray(row))
+            throw new Error("iterateCoveringArray expected an array of arrays but got row " + row);
+        row.forEach(function (y, yindex) {
+            if (y) {
+                func(xindex, yindex);
+            }
+        });
+    });
+};
+var doesCollide = function (x, y) {
+    var ret = false;
+    iterateCoveringArray(currentPiece.shape, function (xindex, yindex) {
+        var xamount = xindex + Math.floor(x / pixel);
+        var yamount = yindex + Math.floor(y / pixel);
+        if (yamount > pixelHeight)
+            ret = true;
+        if (safeGet(safeGet(board, xamount), yamount))
+            return ret = true;
+    });
+    return ret;
+};
+var transpose = function (array) { return array[0].map(function (_, colIndex) { return array.map(function (row) { return row[colIndex]; }); }); };
+function tick() {
+    if (transpose(board)[0].some(function (x) { return x; })) {
+        console.log(board[0]);
+        board = makeBoard();
+    }
+    if (keys.d)
+        currentPiece.position.x = mod(currentPiece.position.x + 3, width - 10);
+    if (keys.s)
+        currentPiece.position.y = mod(currentPiece.position.y + 7, height - 10);
+    if (keys.a)
+        currentPiece.position.x = mod(currentPiece.position.x - 3, width - 10);
+    if (keys.w)
+        currentPiece.position.y = mod(currentPiece.position.y - 3, height - 10);
+    currentPiece.position.y += momevement;
+    var collides = doesCollide(currentPiece.position.x, currentPiece.position.y);
+    if (collides) {
+        console.log("collides!");
+        currentPiece.shape.forEach(function (row, xindex) {
+            row.forEach(function (y, yindex) {
+                if (y) {
+                    // console.log(`setting (${xindex + Math.floor(currentPiece.position.x / pixel)},${yindex + Math.floor(currentPiece.position.y / pixel)} )`)
+                    if (board[(xindex + Math.floor(currentPiece.position.x / pixel))] !== undefined)
+                        board[(xindex + Math.floor(currentPiece.position.x / pixel))][(yindex + Math.floor(currentPiece.position.y / pixel)) - 1] = true;
+                }
+            });
+        });
+        // console.log("boad", bo ard)
+        currentPiece = generatePiece(input.value);
+    }
+    else {
     }
 }
 function main() {
     setInterval(function () {
         tick();
         draw();
-    }, 1e2);
+    }, 30);
 }
 main();
--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement
+const ctx = canvas.getContext("2d");
 const input = document.getElementById("input") as HTMLInputElement;
-const pixel = 10
-const width = 500;
-const height = 500;
-const pixelWidth = 500 / pixel;
-const pixelhHeight = 500 / pixel;
+const pixel = 20
+const pixelWidth = 10;
+const pixelHeight = 24;
+const width = pixelWidth * pixel;
+const height = pixelHeight * pixel;
+
+const momevement = 3
 canvas.width = width;
 canvas.height = height;
 type shape = boolean[][]
@@ -17,66 +20,158 @@ type piece = {
     position: position,
     shape: shape,
 }
-const generatePiece = (): piece => {
+const generateShape = (n: number): shape => {
+    if (n === 1) {
+        return [[true]]
+    } else {
+        let s = generateShape(n - 1)
+        if (Math.random() > 1 / n) {
+            s[Math.floor(Math.random() * s.length)].push(true)
+        } else {
+            s.push([true])
+        }
+        return s;
+    }
+}
+const generatePiece = (n: number = 4): piece => {
     return {
         position: {
             x: 0,
             y: 0,
         },
-        shape: [
-            // [true, true, true],
-            [true, true, true, true, true, true, true]
-        ]
+        shape: generateShape(n)
     }
 }
 let currentPiece: piece = generatePiece();
-
-const makeArray = <T>(length: number, element: T): T[] => new Array(length).fill(element)
-let board = makeArray(pixelWidth, makeArray(pixelhHeight, false));
-function draw() {
-    const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    board.forEach((xrow, xindex) => {
-        xrow.forEach((y, yindex) => {
-            // const x = xrow[xindex];
-            if (y)
-                ctx.fillRect(
-                    xindex * pixel,
-                    yindex * pixel,
-                    pixel,
-                    pixel,
-                )
-            ctx.fillStyle = `hsl(${(yindex * pixel / pixelWidth) * 20}, ${(xindex * pixel / pixelWidth) * 2}%, 50%)`
-
-        })
-    })
-    ctx.fillStyle = "red"
-    currentPiece.shape.forEach((row, rowIndex) => {
-        row.forEach((column, columnIndex) => {
-            if (column)
-                ctx.fillRect(
-                    currentPiece.position.x + columnIndex * pixel,
-                    currentPiece.position.y + rowIndex * pixel,
-                    pixel,
-                    pixel,
-                );
-        })
-    })
-
-    ctx.fillStyle = "black"
-    return canvas;
-}
-function tick() {
-    if (currentPiece.position.y / pixel + pixel > pixelhHeight) {
-        currentPiece = generatePiece();
+const mod = (num: number, base: number): number => {
+    if (num < 0) {
+        return mod(num + base, base)
     } else {
-        currentPiece.position.y += pixel
+        return num % base
+    }
+}
+const safeGet = <T>(array: T[], index: number): T => {
+    if (array.length === 0) {
+        throw new Error(`cannot get an empty array`)
+    }
+    return array[mod(Math.floor(index), array.length)]
+}
+const clone = <T>(x: T): T => {
+    if (Array.isArray(x)) {
+        //@ts-ignore
+        return [...x]
+    } else if (typeof x === "object") {
+        return { ...x }
+    } else {
+        return x
+    }
+}
+const keys = {
+    "w": false,
+    "a": false,
+    s: false,
+    d: false,
+}
+window.addEventListener("keydown", (keyboardEvent) => {
+    const { key } = keyboardEvent
+    if (Object.keys(keys).includes(keyboardEvent.key)) {
+        keys[key] = true
+    }
+})
+window.addEventListener("keyup", (keyboardEvent) => {
+    const { key } = keyboardEvent
+    if (Object.keys(keys).includes(keyboardEvent.key)) {
+        keys[key] = false
+    }
+})
+const makeArray = <T>(length: number, element: T): T[] => {
+    let x: T[] = []
+    for (let i = 0; i < length; i++) {
+        x.push(clone(element))
+    }
+    console.log(x)
+    return x;
+}
+const drawPixel = (x: number, y: number): void => ctx.fillRect(
+    x,
+    y,
+    pixel,
+    pixel,
+)
+const makeBoard = (): boolean[][] => makeArray(pixelWidth, [...makeArray(pixelHeight - 1, false), ...makeArray(pixelWidth, true)]);
+let board: boolean[][] = makeBoard();
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    iterateCoveringArray(board, (xindex, yindex) => {
+        ctx.fillStyle = `hsl(${(yindex / pixelHeight) * 360}, ${(xindex / pixelWidth) * 100}%, 60%)`
+        drawPixel(xindex * pixel, yindex * pixel)
+    })
+    // ctx.fillStyle = "red"
+    iterateCoveringArray(currentPiece.shape, (columnIndex, rowIndex) =>
+        drawPixel(
+            currentPiece.position.x + columnIndex * pixel,
+            currentPiece.position.y + rowIndex * pixel,
+        )
+    )
+}
+const iterateCoveringArray = (piece: boolean[][], func: (xindex: number, yindex: number) => any): void => {
+    if (!Array.isArray(piece)) throw new Error(`iterateCoveringArray expected an array but got ${piece}`)
+    piece.forEach((row, xindex) => {
+        if (!Array.isArray(row)) throw new Error(`iterateCoveringArray expected an array of arrays but got row ${row}`)
+        row.forEach((y, yindex) => {
+            if (y) {
+                func(xindex, yindex)
+            }
+        })
+    })
+}
+const doesCollide = (x: number, y: number): boolean => {
+    let ret = false
+    iterateCoveringArray(currentPiece.shape, (xindex, yindex) => {
+        const xamount = xindex + Math.floor(x / pixel)
+        const yamount = yindex + Math.floor(y / pixel)
+        if (yamount > pixelHeight) ret = true
+        if (safeGet(safeGet(board, xamount), yamount)) return ret = true
+    })
+    return ret
+}
+const transpose = <T>(array: T[][]): T[][] => array[0].map((_, colIndex) => array.map(row => row[colIndex]));
+
+function tick() {
+    if (transpose(board)[0].some(x => x)) {
+        console.log(board[0])
+        board = makeBoard()
+    }
+    if (keys.d) currentPiece.position.x = mod(currentPiece.position.x + 3, width - 10)
+    if (keys.s) currentPiece.position.y = mod(currentPiece.position.y + 7, height - 10)
+    if (keys.a) currentPiece.position.x = mod(currentPiece.position.x - 3, width - 10)
+    if (keys.w) currentPiece.position.y = mod(currentPiece.position.y - 3, height - 10)
+
+    currentPiece.position.y += momevement
+    const collides = doesCollide(currentPiece.position.x, currentPiece.position.y)
+    if (collides) {
+        console.log("collides!")
+        currentPiece.shape.forEach((row, xindex) => {
+            row.forEach((y, yindex) => {
+                if (y) {
+                    // console.log(`setting (${xindex + Math.floor(currentPiece.position.x / pixel)},${yindex + Math.floor(currentPiece.position.y / pixel)} )`)
+                    if (board[(xindex + Math.floor(currentPiece.position.x / pixel))] !== undefined)
+                        board[(xindex + Math.floor(currentPiece.position.x / pixel))]
+                        [(yindex + Math.floor(currentPiece.position.y / pixel)) - 1] = true
+                }
+            })
+        })
+        // console.log("boad", bo ard)
+        currentPiece = generatePiece(input.value);
+
+    } else {
+
     }
 }
 function main() {
     setInterval(() => {
         tick();
         draw();
-    }, 1e2)
+    }, 30)
 }
 main();
